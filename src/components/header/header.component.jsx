f6ed954a1940607bf5d5eb05e303a2f6a352bc7f
anonymous <prakashsanyasi619@gmail.com>
2020-11-07 22:27:30 +0600

    Add checkout page + use selector

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,6 +9,9 @@ import CartIcon from '../cart-icon/cart-icon.componet';
 import CartDropdown from '../cart-dropdown/cart-dropdown.compenent';
 
 import './header.styles.scss';
+import { createStructuredSelector } from 'reselect';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 const Header = ({ currentUser, hidden }) => (
     <div className='header'>
@@ -39,9 +42,9 @@ const Header = ({ currentUser, hidden }) => (
     </div>
 )
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({
-    currentUser,
-    hidden
-})
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
+});
 
 export default connect(mapStateToProps)(Header);
