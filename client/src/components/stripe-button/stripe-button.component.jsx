2432ef0baa5ed9789b5ade2bca8e90ebcc2dbf8b
anonymous <prakashsanyasi619@gmail.com>
2020-12-26 19:22:59 +0600

    Add backend sever for stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51HldK6FOzWm7OUmC9rhWgh79BLwfjs9A78i9k1pbo7q16CMjqq00Y24AO6o3rORG5ZG55YtgUhwGnvFek1tlftzI00sdvi07Qx'
 
     const onToken = token => {
-        alert('Payment Successful');
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            } 
+        }).then(response => {
+            alert('Payment Successful');
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error));
+            alert('There was an issue with your payment. Please make sure you use the provided credit card');
+        })
     }
 
     return (
